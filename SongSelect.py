67d6b9613136b7ffd7cd76020b9a7b84ce49cc03
Taha Ugurcan Ongel <tahagurcanoffical@gmail.com>
2022-12-17 15:41:22 +0300

    add a OptionMenu for users to logout, it doesn't work as intended

--- SongSelect.py
+++ SongSelect.py
@@ -1,4 +1,5 @@
-from customtkinter import CTkLabel, CTkButton, CTkFrame
+from customtkinter import CTkLabel, CTkButton, CTkFrame, CTkScrollbar
+import tkinter
 
 from music import Music
 from player import Player
@@ -10,10 +11,11 @@ class SongSelect(CTkFrame):
         self.mainWindow = main_window
         self.currentMusic = None
         self.player = player
+        rowcount = 0
         for music, rowcount in zip(music_list, range(len(music_list))):
             if music.music_name is None:
                 self.info_label = CTkLabel(text="Kütüphanenizde Hiç Şarkı Yok")
-                self.info_label.grid(row=0, column=0, padx=2.5, pady=2.5)
+                self.info_label.grid(row=0, column=0, padx=2.5, pady=2.5, sticky="w")
                 return
             self.button = CTkButton(
                 master=self,
